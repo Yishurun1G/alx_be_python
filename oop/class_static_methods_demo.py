656a31279f1c491183bed56bfefd7665dd65904a
Y <yishurung@gmail.com>
2025-08-10 23:32:41 +0300

    Create class_static_methods_demo.py

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,16 @@
+
+
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        """Static method: performs addition without accessing class data"""
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        """Class method: accesses class attribute and performs multiplication"""
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
